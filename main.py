981b9c2f3eeba1e69e073e0f8f6a4675817f9a87
Dartty123 <dariyatamanuk@icloud.com>
2025-04-05 12:05:37 +0300

    save

--- main.py
+++ main.py
@@ -10,27 +10,6 @@ def index():
     return dict(msg="Hello World")
 
 
-@app.get("/name/")
-def get_name(   
-    name: str = Query("Ви не ввели ім'я", description="Введіть своє ім'я"),
-    age: int = Query(0, description="Введіть свій вік")
-):
-    return dict(    
-        msg=f"Вітаю '{name}' в нашій інформаційній системі",
-        age=age,
-        age_type=f"{type(age)}"
-        )
-
-
-@app.get("/info/")
-def info(   
-    city: str = Query("Ви неввели місто", description="Введіть назву міста"),
-    region: str = Query("Не ввели назву області", description="Введіть назву област")
-):
-    return dict(    
-        msg=f"Вітаю ось ваше {city} а ось ваша область {region}"
-    )
-
 @app.get("/calculator/")
 def calculator(extion: str=Query ( description="Виберіть операцію + - / * "), num_1: int = Query(description="Введіть перше число"), num_2: int=Query(description="Введіть друге число"), ):
     if extion == "plus":
